d8a61d35b1c6343ff2f700d534a764fd3ada5386
Rodrigo UWaterloo <raraujo@Macintosh.local>
2015-03-28 15:39:57 -0400

    Add the print message

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorldProject
+//
+//  Created by Rodrigo UWaterloo on 2015-03-28.
+//  Copyright (c) 2015 Rodrigo Araujo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello World!\n";
+    return 0;
+}
